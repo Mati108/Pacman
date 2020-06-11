0ccb8f7dab0ecce7bf44508265a48a332c700c22
Mati108 <62069366+Mati108@users.noreply.github.com>
2020-06-11 16:34:21 +0200

    Initial Commit: Basic structure of the game.

---
+++ src/Apple.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Apple extends Rectangle {
+
+    private static final long serialVersionUID = 1L;
+
+    public Apple(int x, int y){
+        setBounds(x + 10, y + 8, 8, 8);
+    }
+
+    public void render(Graphics g){
+        g.setColor(Color.green);
+        g.fillRect(x, y, width, height);
+    }
+}
---
+++ src/Level.java
@@ -0,0 +1,76 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+public class Level {
+
+    public int width;
+    public int height;
+
+    public Tile[][] tiles;
+
+    public List<Apple> apples;
+    public List<Enemy> enemies;
+
+    public Level(String path) {
+        apples = new ArrayList<>();
+        enemies = new ArrayList<>();
+        try {
+            System.out.print("Try");
+            BufferedImage map = ImageIO.read(getClass().getResource("map.png"));
+            this.width = map.getWidth();
+            this.height = map.getHeight();
+            int[] pixels = new int[width * height];
+            tiles = new Tile[width][height];
+            map.getRGB(0, 0, width, height, pixels, 0, width);
+
+            for (int xx = 0; xx < this.width; xx++) {
+                for (int yy = 0; yy < this.height; yy++) {
+                    int val = pixels[xx + (yy * width)];
+
+                    if (val == 0xFF000000) {
+                        // Tile
+                        tiles[xx][yy] = new Tile(xx * 32, yy * 32);
+                    } else if (val == 0xFF0000FF) {
+                        // Player
+                        Game.player.x = xx * 32;
+                        Game.player.y = yy * 32;
+                    } else if (val == 0xFFFF0000){
+                        // Enemy
+                        enemies.add(new Enemy(xx*32, yy*32));
+                    } else{
+                        apples.add(new Apple(xx*32, yy*32));
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    public void tick(){
+        for (int i = 0; i < enemies.size(); i++){
+            enemies.get(i).tick();
+        }
+    }
+
+    public void render(Graphics g) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (tiles[x][y] != null) tiles[x][y].render(g);
+            }
+        }
+        for (int i = 0; i < apples.size(); i++){
+            apples.get(i).render(g);
+        }
+        for (int i = 0; i < enemies.size(); i++){
+            enemies.get(i).render(g);
+        }
+    }
+}
+
