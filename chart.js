7d1e5695c52579bbc450274a646532d89ecc6e58
Mati108 <62069366+Mati108@users.noreply.github.com>
2021-02-02 09:07:32 +0100

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,20 +1,40 @@
-function customAccessor(data) {
-  const newData = [];
+var data = [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
 
-  data.forEach(function(point, index) {
-    newData.push([point.date, point.value]);
-  });
+var dataMax = Math.max.apply(null, data)
+var dataMin = Math.min.apply(null, data)
+var range = dataMax - dataMin
+var areaMax = 20
+var areaMin = 2
 
-  return newData;
-}
+var mappedData = data.map(v => {
+  var relSize = ((v - dataMin) / range)
+  var radius = Math.floor(areaMin + relSize * (areaMax - areaMin))
+
+  return {
+    y: v,
+    marker: {
+      radius: (radius)
+    }
+  }
+})
 
 Highcharts.chart('container', {
+  chart: {
+    type: 'scatter'
+  },
+  xAxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+
   plotOptions: {
     series: {
-      keys: ['x', 'y']
+      marker: {
+        radius: 8
+      }
     }
   },
+
   series: [{
-    data: customAccessor(data)
+    data: mappedData
   }]
 });
